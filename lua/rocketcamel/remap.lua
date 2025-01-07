a957e830107178f743f04987ce37627ec501f760
rocketcamel <87954986+rocketcamel@users.noreply.github.com>
2025-01-06 23:21:06 -0800

    Move cursor in insert mode

--- lua/rocketcamel/remap.lua
+++ lua/rocketcamel/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>x", vim.cmd.q)
 vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = false })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
